31f19d68ba6c71c5bcb0859b23b9b9894013186a
Hamel Husain <hamel.husain@gmail.com>
2023-03-25 12:50:33 -0700

    update

---
+++ lib/index.d.ts
@@ -0,0 +1,6 @@
+import { JupyterFrontEndPlugin } from '@jupyterlab/application';
+/**
+ * Initialization data for the nbdev_ext extension.
+ */
+declare const plugin: JupyterFrontEndPlugin<void>;
+export default plugin;
